6b2a88c13a13758b9fad1ecebe200fa956a02539
YosephAlves <92815723+YosephAlves@users.noreply.github.com>
2022-09-07 18:57:44 +0100

    first commit

---
+++ JS/loader.js
@@ -0,0 +1,9 @@
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+    $(".loading").fadeOut("slow");
+  });
+
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+    $(".loadinghome").fadeOut("slow");
+});
